a399868c697fdd0c16979d94f816efc2164f7c00
arthurmacielsilva123-cmD <arthur.maciel.silva123@gmail.com>
2025-10-30 17:41:15 -0300

    Add files via upload

---
+++ 03_coleções/05_atividade/main.py
@@ -0,0 +1,9 @@
+estados = ["Acre","Alagoas", "Amazonas", "Bahia", "Ceará", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais","Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina","São Paulo", "Sergipe", "Tocantins"]
+
+print("estados basileiros")
+
+estados.sort()
+
+print("\nEstados Brasileiros:\n")
+for nome in estados:
+    print(nome)
